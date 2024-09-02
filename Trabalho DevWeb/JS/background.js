65d7127cb6d405b9ab5a2d48ea26c39ef119e5c5
Higor B. <104683342+ABNegativo@users.noreply.github.com>
2024-09-02 16:37:23 -0400

    Add files via upload

---
+++ Trabalho DevWeb/JS/background.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById('lottie-background'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/2fd966e4-c77f-4225-a361-682ad974811d/4kLT0nUvMG.json'
+});
